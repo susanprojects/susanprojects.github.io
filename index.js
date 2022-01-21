bad63bacb1bb4dcd511a9792762a4888da359a05
Susanna Gaddam <susansavio13@gmail.com>
2022-01-21 17:43:52 +0100

    fixed issue with css and promise

--- index.js
+++ index.js
@@ -1,14 +1,63 @@
-let video = document.querySelector("#video");
+const video = document.querySelector("#video");
+const selectCameraButton = document.getElementById("btn-select");
+const cameraDevice = document.getElementById("available-cameras");
 
-if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-  navigator.mediaDevices
-    .getUserMedia({ video: true })
-    .then((stream) => {
-      video.srcObject = stream;
-    })
-    .catch((error) => {
-      if (error.name === "NotAllowedError") {
-        console.log("Permission has not been granted to use your camera");
-      }
-    });
+let currentStream;
+let supports = navigator.mediaDevices.getSupportedConstraints();
+
+if (supports["facingMode"] === true) {
+  selectCameraButton.disabled = false;
 }
+
+function pauseMediaTrack(stream) {
+  stream.getTracks().forEach((currentMediaTrack) => {
+    currentMediaTrack.stop();
+  });
+}
+
+function fetchDevices(mediaDevices) {
+  let count = 1;
+  mediaDevices.forEach((mediaDevice) => {
+    if (mediaDevice.kind === "videoinput") {
+      const div = document.createElement("div");
+      const node = document.createTextNode(
+        mediaDevice.label || `Camera ${count++}`
+      );
+      div.appendChild(node);
+      cameraDevice.appendChild(div);
+    }
+  });
+}
+
+selectCameraButton.addEventListener("click", () => {
+  if (typeof currentStream !== "undefined") {
+    pauseMediaTrack(currentStream);
+  }
+
+  const videoConstraints = {};
+  if (cameraDevice.value === "") {
+    videoConstraints.facingMode = "environment";
+  } else {
+    videoConstraints.deviceId = cameraDevice.value;
+  }
+
+  if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+    const constraints = {
+      audio: false,
+      video: videoConstraints,
+    };
+    startVideoStreaming(constraints);
+  }
+});
+
+const startVideoStreaming = async (constraints) => {
+  try {
+    const stream = await navigator.mediaDevices.getUserMedia(constraints);
+    video.srcObject = stream;
+    video.play();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+navigator.mediaDevices.enumerateDevices().then(fetchDevices);
